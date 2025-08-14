9a49f5e443e9ad37ea3628ce99db202d2b579bab
Marcus <1175677541@qq.com>
2025-08-15 00:17:49 +0800

    initial commit

---
+++ lines.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INCLUDE_DIR="./include"
+
+TOTAL_LINES=0
+
+while IFS= read -r -d $'\0' file; do
+    LINE_COUNT=$(wc -l < "$file")
+    TOTAL_LINES=$((TOTAL_LINES + LINE_COUNT))
+
+    echo "  $LINE_COUNT lines: $file"
+done < <(find "$INCLUDE_DIR" -name "*.hpp" -print0)
+
+echo "----------------------------------------"
+echo "total：$TOTAL_LINES lines"
+
+exit 0
